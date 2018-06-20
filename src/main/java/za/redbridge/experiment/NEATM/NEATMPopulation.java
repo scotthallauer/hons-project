691ba99f12cbad4d6936d5cf6cd81b64b735ca41
alex <alex.cleo@gmail.com>
2018-06-20 16:22:41 +0200

    FEAT(NEATPopulation): extend reset() method to account for HyperNEATM being used.

--- src/main/java/za/redbridge/experiment/NEATM/NEATMPopulation.java
+++ src/main/java/za/redbridge/experiment/NEATM/NEATMPopulation.java
@@ -1,11 +1,18 @@
 package za.redbridge.experiment.NEATM;
 
 import org.encog.ml.ea.species.BasicSpecies;
+import org.encog.neural.hyperneat.FactorHyperNEATGenome;
+import org.encog.neural.hyperneat.HyperNEATCODEC;
+import org.encog.neural.hyperneat.HyperNEATGenome;
+import org.encog.neural.hyperneat.substrate.Substrate;
+import org.encog.neural.neat.FactorNEATGenome;
+import org.encog.neural.neat.NEATCODEC;
 import org.encog.neural.neat.training.NEATGenome;
 import org.encog.neural.neat.training.NEATInnovationList;
 
 import java.util.Random;
 
+import za.redbridge.experiment.HyperNEATM.HyperNEATMCODEC;
 import za.redbridge.experiment.NEATM.sensor.SensorType;
 import za.redbridge.experiment.NEAT.NEATPopulation;
 
@@ -26,7 +33,7 @@ public class NEATMPopulation extends NEATPopulation
     }
 
     /**
-     * Construct a starting NEAT population. This does not generate the initial
+     * Construct a starting NEATM population. This does not generate the initial
      * random population of genomes.
      *
      * @param outputCount    The output neuron count.
@@ -37,11 +44,39 @@ public class NEATMPopulation extends NEATPopulation
         super(SensorType.values().length, outputCount, populationSize); // inputCount = SensorType.values().length
     }                                                                   // This is because initial population begins with one of each sensor type.
 
+
+    /**
+     * Construct a starting HyperNEATM population. This does not generate the
+     * initial random population of genomes.
+     *
+     * @param theSubstrate
+     *            The substrate ID.
+     * @param populationSize
+     *            Number of CPPNs in each generation.
+     */
+    public NEATMPopulation(final Substrate theSubstrate, final int populationSize)
+    {
+        super(theSubstrate, populationSize);
+        setInputCount(getSubstrate().getInputCount());
+        setOutputCount(getSubstrate().getOutputCount());
+    }
+
+
     @Override
     public void reset()
     {
-        setCODEC(new NEATMCODEC());
-        setGenomeFactory(new FactorNEATMGenome());
+        // create the genome factory
+        if (isHyperNEAT())                                  // Just checks if NEAT(M)Population has a valid substrate.
+        {
+            setCODEC(new HyperNEATMCODEC());
+            setGenomeFactory(new FactorHyperNEATGenome());
+        }
+        else
+        {
+            setCODEC(new NEATMCODEC());
+            setGenomeFactory(new FactorNEATMGenome());
+        }
+
 
         // create the new genomes
         getSpecies().clear();
