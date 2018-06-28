9555454f4b691055e6115b9256bf0256fdf5e623
alex <alex.cleo@gmail.com>
2018-06-28 17:33:09 +0200

    UPDATE: Add cases for LowResCameraSensor and ColourProximitySensor

--- src/main/java/za/redbridge/experiment/NEATM/sensor/SensorMorphology.java
+++ src/main/java/za/redbridge/experiment/NEATM/sensor/SensorMorphology.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.Arrays;
 
 import za.redbridge.simulator.khepera.BottomProximitySensor;
+import za.redbridge.simulator.khepera.LowResCameraSensor;
 import za.redbridge.simulator.khepera.ProximitySensor;
 import za.redbridge.simulator.khepera.UltrasonicSensor;
 import za.redbridge.simulator.sensor.AgentSensor;
@@ -44,7 +45,12 @@ public class SensorMorphology implements Serializable {
             case ULTRASONIC:
                 return new UltrasonicSensor(config.getBearing(), config.getOrientation(),
                         config.getRange(), config.getFieldOfView());
-
+            case LOW_RES_CAM:
+                return new UltrasonicSensor(config.getBearing(), config.getOrientation(),
+                        config.getRange(), config.getFieldOfView());
+            case COLOUR_PROXIMITY:
+                return new UltrasonicSensor(config.getBearing(), config.getOrientation(),
+                        config.getRange(), config.getFieldOfView());
         }
 
         return null;
